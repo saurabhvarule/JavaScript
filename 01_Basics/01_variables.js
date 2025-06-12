ee9077b20059ea644a6459b0f44b6f5778c3870e
Saurabh Varule <varulesaurabh44@gmail.com>
2025-06-12 14:07:53 +0000

    variables

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -4,6 +4,7 @@ let accountEmail = "sv@sv@gmail.com"
 var accountPass = "18y4r7"
 accountCity = "Malegaon"   // we can also declare variable like this  just for your knowledge purpose
 
+
 // accountId = 12321   --> not allowed
 accountEmail = "saurabhvarule@gmail.com"
 accountPass = "Malegaon@41"
