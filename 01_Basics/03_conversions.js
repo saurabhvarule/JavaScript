45cb9676b20baa6b056443f424537c2d976170f1
Saurabh Varule <varulesaurabh44@gmail.com>
2025-06-13 07:37:53 +0000

    conversions

---
+++ 01_Basics/03_conversions.js
@@ -0,0 +1,25 @@
+
+let name = "Saurabh";
+let place = new String("            P-une");
+
+console.log(`The name is ${name} and its type is ${typeof name}`);
+console.log(`Lives in ${place} and its type is ${typeof place}`);
+
+place.trimStart(' ');
+console.log(`Lives in ${place} and its type is ${typeof place}`);
+
+let value = 2;
+let negValue = -value;
+
+console.log(value);
+console.log(negValue);
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 % 2);
+console.log(2 + '2' + '1');
+console.log('2' + '1' + 1);
+console.log(2 + 3 + '1');
+
+
