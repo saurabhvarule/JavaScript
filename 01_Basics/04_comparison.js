98ba22bd5b73e1418c35ced42c1d981e8d045412
Saurabh Varule <varulesaurabh44@gmail.com>
2025-06-13 07:38:16 +0000

    comparisons

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,28 @@
+
+// normal comaparison
+console.log("Normal Comparison");
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+console.log("02" > 1);
+console.log("02" < 1);
+console.log("02" < "1");
+console.log();
+
+// null
+console.log("null");
+console.log(null > 0);
+console.log(null < 0);
+console.log(null == 0);
+console.log();
+
+
+// undefined
+console.log("Undefined");
+console.log(null > 0);
+console.log(null > 0);
+console.log(null > 0);
+
